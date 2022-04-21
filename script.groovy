8dfecd868cb337e9d0b868f692c5c74de0aa2e76
RaffBalayan <rafikbalayan@gmail.com>
2022-04-21 20:42:33 +0400

    Create script.groovy
    first script for groovy

---
+++ script.groovy
@@ -0,0 +1,19 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'Nexus-Jenkins', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t 3.226.122.151:8083:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin  18.204.220.253:8083 "
+        sh 'docker push 3.226.122.151:8083:jma-2.0'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
