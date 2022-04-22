958911d437e9fb3917d14d8509b5f1b2d4036137
RaffBalayan <rafikbalayan@gmail.com>
2022-04-22 11:52:52 +0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'Nexus-Jenkins', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 3.226.122.151.8083:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin  18.204.220.253:8083 "
-        sh 'docker push 3.226.122.151.8083:jma-2.0'
+    withCredentials([usernamePassword(credentialsId: 'For-DockerHub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t r723/tets:1.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin  "
+        sh 'docker push r723/tets:1.0'
     }
 } 
 
